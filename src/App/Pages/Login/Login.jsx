529020b26cb1ead6952cf0457f36c4b4c5c491d8
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-30 17:17:18 +0700

    Gonfiguring Redux and Firebase

--- src/App/Pages/Login/Login.jsx
+++ src/App/Pages/Login/Login.jsx
@@ -3,6 +3,30 @@ import './StyleLogin.css'
 
 
 export default function Login() {
+
+    // const signIn = (selectedProvider) => {
+    //     socialLogin(selectedProvider);
+    // };
+
+    // const dispatch = useDispatch();
+    // const socialLogin = async (selectedProvider) => {
+    //     let provider;
+    //     const auth = firebase.auth();
+    //     if (selectedProvider === "facebook") {
+    //     provider = new firebase.auth.FacebookAuthProvider();
+    //     }
+    //     if (selectedProvider === "google") {
+    //     provider = new firebase.auth.GoogleAuthProvider();
+    //     }
+    //     try {
+    //     const result = await auth.signInWithPopup(provider);
+    //     console.log(result.user);
+    //     dispatch(userSignIn(result.user));
+    //     } catch (error) {
+    //     console.log(error);
+    //     }
+    // };
+
     return (
         <div>
             <h1>Hello I am the Login Page </h1>
