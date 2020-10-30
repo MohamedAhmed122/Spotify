0c89b307d816b19039822c3b48588abdf5080089
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-30 22:47:18 +0700

    Set The Player Pages Up And Ready To GO0o0o0

--- src/App/Pages/MainPage/MainPage.jsx
+++ src/App/Pages/MainPage/MainPage.jsx
@@ -2,28 +2,37 @@ import React, { useEffect, useState } from 'react'
 
 import Login from '../Login/Login'
 import { getTokenFromResponse } from '../Login/SpotifyLogin'
-
+import PlayerPage from '../PlayerPage/PlayerPage';
+var Spotify = require('spotify-web-api-js');
 
 
 export default function MainPage() {
 
     const [token, setToken] = useState(null)
 
+    var spotify = new Spotify();
+
    useEffect(()=>{
        const hash = getTokenFromResponse();
        window.location.hash = '';
        const _token = hash.access_token;
-       console.log(_token);
+     
        if(_token){
            setToken(_token);
+
+           spotify.setAccessToken(_token);
+           
+           spotify.getMe().then(user =>{
+               console.log('user' , user);
+           });
        }
-   },[token])
+   },[token, spotify])
 
 
     return (
         <div>
            {
-               !token? <Login />: <h1>Login is done </h1>
+               !token? <Login />: <PlayerPage />
            }
         </div>
     )
