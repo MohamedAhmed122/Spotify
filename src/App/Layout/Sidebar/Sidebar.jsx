6137f26ea5162b8a3474c68a591bed6652fa2a26
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-31 01:05:10 +0700

    Sidebar is Totally Finished

--- src/App/Layout/Sidebar/Sidebar.jsx
+++ src/App/Layout/Sidebar/Sidebar.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import SidebarOption from './SidebarOption/SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 import './StyleSidebar.css';
 
@@ -10,6 +14,15 @@ export default function Sidebar() {
             src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
             alt=""
             />
+            <SidebarOption Icon={HomeIcon}  title='Home' />
+            <SidebarOption Icon={SearchIcon} title='Search' />
+            <SidebarOption Icon={LibraryMusicIcon} title='Your Library' />
+            <br />
+            <strong>PLAYLIST</strong>
+            <hr />
+            <SidebarOption title='Home' />
+            <SidebarOption  title='Search' />
+            <SidebarOption title='Your Library' />
         </div>
     )
 }
--- src/App/Layout/Sidebar/SidebarOption/SidebarOption.jsx
+++ src/App/Layout/Sidebar/SidebarOption/SidebarOption.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './StyleSidebarOption.css'
 
-export default function SidebarOption({title, icon}) {
+export default function SidebarOption({title, Icon}) {
     return (
         <div className='SidebarOption'>
-            <p>{title}</p>
+            {Icon && <Icon  className='sidebar_icon'/>}
+           {Icon? <p style={{fontSize: 19}}>{title}</p> : <p className='title'>{title}</p>}
         </div>
     )
 }
